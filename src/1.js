bb9318068bb00cf2778b6f5d7634685a05967656
hkc <779543729@qq.com>
2018-09-10 09:08:57 +0800

    no.1

---
+++ src/1.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    var i = 0, j = 1, length = nums.length;
+    for (; i < length; i++) {
+        for (j = i + 1; j < length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j]
+            }
+        }
+    }
+};
